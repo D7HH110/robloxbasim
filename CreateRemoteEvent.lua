f3701e5277bdeaf150962dd30592142396f5cbfc
D7HH110 <d7h202@gmail.com>
2025-08-20 22:23:50 +0300

    Delete CreateRemoteEvent.lua

--- CreateRemoteEvent.lua
+++
@@ -1,8 +0,0 @@
--- CreateRemoteEvent.lua
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-if not ReplicatedStorage:FindFirstChild("SpawnPetEvent") then
-    local event = Instance.new("RemoteEvent")
-    event.Name = "SpawnPetEvent"
-    event.Parent = ReplicatedStorage
-end
